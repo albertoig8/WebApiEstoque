103c5663455e57b1f1b12dd499aa1818e455b80a
Gabriel Baruque <baruque@edu.unirio.br>
2021-10-25 17:19:47 -0300

    Incremento de pacotes Nuget, criação de pastas model e Data.

---
+++ WebApiEstoque/Data/ProdutoContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApiEstoque.Data
+{
+    public class ProdutoContext : DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("");
+        }
+    }
+
+    
+}
