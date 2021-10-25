ae6d2b5a131832be6dceed648d5a8f842af9b873
Alberto Araujo <albertoig8@hotmail.com>
2021-10-25 10:51:58 -0300

    Criando a classe Produto

---
+++ WebApiEstoque/Produto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiEstoque
+{
+    public class Produto
+    {
+        int ID { get; set; }
+        string nome { get; set; }
+        string modelo { get; set; }
+        string cor { get; set; }
+        double tamanho { get; set; }
+        int quantidade { get; set; }
+        string imageUrl { get; set; }
+    }
+}
